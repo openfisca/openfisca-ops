68eb37ac40ead1eff2da99984c74f93a6a2d32d3
Christophe Benz <christophe.benz@gmail.com>
2017-01-25 20:32:09 +0100

    Use pip ability to skip dev packages

--- auto-update-pip-packages/update-openfisca-virtualenv.sh
+++ auto-update-pip-packages/update-openfisca-virtualenv.sh
@@ -1,31 +1,21 @@
 #!/bin/bash
 
-set -e
-
 
 # Customizable variables
-
 VENV_ACTIVATE="$HOME/virtualenvs/openfisca/bin/activate"
-
 # End of customizable variables
 
-
 CURL="curl --silent --retry 5"
+restart_services="0"
 
-restart="0"
-
-function check_and_update {
+function upgrade_package {
     PACKAGE_NAME="$1"
-    PACKAGE_JSON_URL="https://pypi.python.org/pypi/$PACKAGE_NAME/json"
-    PACKAGE_PYPI_VERSION=`$CURL "$PACKAGE_JSON_URL" | jq --raw-output ".info.version"`
-    PACKAGE_PIP_VERSION=`python -c "import pkg_resources; print(pkg_resources.get_distribution('$PACKAGE_NAME').version)"`
-    if [[ "$PACKAGE_PYPI_VERSION" != "$PACKAGE_PIP_VERSION" ]]; then
-        # Check for X.Y.Z version number, exclude alpha, rc, dev versions.
-        echo "$PACKAGE_PYPI_VERSION" | grep "^[0-9]\+\.[0-9]\+\.[0-9]\+$" > /dev/null
-        if [ $? == 0 ]; then
-            pip install --no-cache-dir --upgrade $PACKAGE_NAME
-            restart="1"
-        fi
+    CURRENT_VERSION=`python -c "import pkg_resources; print(pkg_resources.get_distribution('$PACKAGE_NAME').version)"`
+    pip install --quiet --no-cache-dir --upgrade $PACKAGE_NAME
+    NEW_VERSION=`python -c "import pkg_resources; print(pkg_resources.get_distribution('$PACKAGE_NAME').version)"`
+    if [[ "$NEW_VERSION" != "$CURRENT_VERSION" ]]; then
+        echo "- New version of package $PACKAGE_NAME was found on PyPI: current=$CURRENT_VERSION, new=$NEW_VERSION"
+        restart_services="1"
     fi
 }
 
@@ -33,16 +23,14 @@ source $VENV_ACTIVATE
 
 for package in OpenFisca-France OpenFisca-Core OpenFisca-Web-API
 do
-    check_and_update "$package"
+    upgrade_package "$package"
 done
 
-if [ "$restart" == "1" ]; then
-    echo "========="
-    echo "New version(s) of PyPI package(s) were found and installed."
-    echo "Restarting openfisca-web-api.service and legislation-explorer.service as a consequence."
-    echo "========="
+if [ "$restart_services" == "1" ]; then
+    echo "- New version(s) of PyPI package(s) were found and installed. Restarting openfisca-web-api.service and legislation-explorer.service as a consequence."
     sudo systemctl restart openfisca-web-api.service
     sudo systemctl restart legislation-explorer.service
     # Trigger an HTTP request to pre-load OpenFisca-Web-API, so the first user will not have to wait.
+    sleep 10
     $CURL https://api.openfisca.fr/
 fi
