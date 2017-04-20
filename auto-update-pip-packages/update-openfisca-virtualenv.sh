ce67f5d7ea068a7ef1ed0da551ac76bf3f9fa40c
Christophe Benz <christophe.benz@gmail.com>
2017-04-20 10:49:55 +0200

    Download requirements.txt before using it

--- auto-update-pip-packages/update-openfisca-virtualenv.sh
+++ auto-update-pip-packages/update-openfisca-virtualenv.sh
@@ -2,10 +2,15 @@
 
 # Adapt this variable to your server.
 VENV_ACTIVATE="/home/openfisca/virtualenvs/openfisca/bin/activate"
+REQUIREMENTS_URL="https://raw.githubusercontent.com/openfisca/openfisca-ops/master/auto-update-pip-packages/requirements.txt"
+REQUIREMENTS="/home/openfisca/openfisca-ops/auto-update-pip-packages/requirements.txt"
 
 set -x
+
+wget "$REQUIREMENTS_URL" --output-document="$REQUIREMENTS"
+
 source "$VENV_ACTIVATE"
-pip install --requirement /home/openfisca/openfisca-ops/auto-update-pip-packages/requirements.txt --upgrade
+pip install --requirement "$REQUIREMENTS" --upgrade
 if ! (pip check); then
     exit -1
 fi
