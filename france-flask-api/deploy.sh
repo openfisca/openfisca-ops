fc9c176cfbe37fcddb2cc2c75da044d5f0a1ea63
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-05-11 11:27:50 +0200

    Remove useless line

--- france-flask-api/deploy.sh
+++ france-flask-api/deploy.sh
@@ -8,7 +8,6 @@ NEW_REQUIREMENTS="/home/openfisca/new-api/requirements.txt"
 sed -e 's/\[api\]//' "$REQUIREMENTS" > "$NEW_REQUIREMENTS"
 
 source /home/openfisca/virtualenvs/new-api/bin/activate
-pip install openfisca_core --upgrade
 pip install --requirement "$NEW_REQUIREMENTS" --upgrade
 # The current user must have been specifically allowed to run the next command
 # Use the visudo command to do so
