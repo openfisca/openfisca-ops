46135a8e6c0e7ee7a122f123981b88ed4981c202
sandcha <sandcha@users.noreply.github.com>
2018-04-09 15:07:17 +0200

    Update api/v21 configuration version

--- fr.openfisca.org/api/v21/deploy.sh
+++ fr.openfisca.org/api/v21/deploy.sh
@@ -2,10 +2,10 @@
 
 set -ex
 
-REQUIREMENTS="/home/openfisca/openfisca-ops/fr.openfisca.org/api/v20/requirements.txt"
+REQUIREMENTS="/home/openfisca/openfisca-ops/fr.openfisca.org/api/v21/requirements.txt"
 
-source /home/openfisca/virtualenvs/api-fr20/bin/activate
+source /home/openfisca/virtualenvs/api-fr21/bin/activate
 pip install --requirement "$REQUIREMENTS" --upgrade
 # The current user must have been specifically allowed to run the next command
 # Use the visudo command to do so
-sudo systemctl restart openfisca-web-api-fr20.service
+sudo systemctl restart openfisca-web-api-fr21.service
