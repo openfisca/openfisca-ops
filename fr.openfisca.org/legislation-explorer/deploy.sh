87647eeeba2f9e41b9c7fe9ef0ffc9273abd84d0
Florian Pagnoux <florian.pagnoux@gmail.com>
2018-08-14 23:45:29 -0400

    Don't pass env var manually

--- fr.openfisca.org/legislation-explorer/deploy.sh
+++ fr.openfisca.org/legislation-explorer/deploy.sh
@@ -1,7 +1,6 @@
 git pull
 npm install
-npm install swagger-ui@3.17.3
 
-NODE_ENV=production PATHNAME=/legislation/  API_URL=https://fr.openfisca.org/api/v22 npm run build
+npm run build
 echo "Restarting legislation-explorer service..."
 sudo systemctl restart legislation-explorer.service
