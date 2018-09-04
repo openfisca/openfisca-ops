4f2815f25060e248b8d18c531b841ac954b21227
Florian Pagnoux <florian.pagnoux@gmail.com>
2018-09-04 16:31:45 -0400

    Rebuild legexp on deployment

--- fr.openfisca.org/api/v24/deploy.sh
+++ fr.openfisca.org/api/v24/deploy.sh
@@ -9,4 +9,4 @@ pip install --requirement "$REQUIREMENTS" --upgrade --upgrade-strategy eager
 # The current user must have been specifically allowed to run the next command
 # Use the visudo command to do so
 sudo systemctl restart openfisca-web-api-fr24.service
-sudo systemctl restart legislation-explorer.service
+/home/openfisca/openfisca-ops/fr.openfisca.org/legislation-explorer/deploy.sh
