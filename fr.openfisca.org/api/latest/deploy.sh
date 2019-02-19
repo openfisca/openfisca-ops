cb1705075eb48927b9d471dc33d135e6b52fbd0a
sandcha <sandcha@users.noreply.github.com>
2019-02-19 11:30:10 +0100

    Delete upgrade eager install for latest api

--- fr.openfisca.org/api/latest/deploy.sh
+++ fr.openfisca.org/api/latest/deploy.sh
@@ -5,7 +5,7 @@ set -ex
 REQUIREMENTS="/home/openfisca/openfisca-ops/fr.openfisca.org/api/latest/requirements.txt"
 
 source /home/openfisca/virtualenvs/api-fr-latest/bin/activate
-pip install --requirement "$REQUIREMENTS" --upgrade --upgrade-strategy eager
+pip install --requirement "$REQUIREMENTS"
 # The current user must have been specifically allowed to run the next command
 # Use the visudo command to do so
 sudo systemctl restart openfisca-web-api-fr-latest.service
