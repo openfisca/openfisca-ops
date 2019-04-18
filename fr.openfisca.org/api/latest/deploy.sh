72e919fde4d6744cf1f22d4d61a3b345dc4c5766
Florian Pagnoux <florian.pagnoux@gmail.com>
2019-04-18 12:01:22 -0400

    Ensure compat france core

--- fr.openfisca.org/api/latest/deploy.sh
+++ fr.openfisca.org/api/latest/deploy.sh
@@ -2,10 +2,12 @@
 
 set -ex
 
-REQUIREMENTS="/home/openfisca/openfisca-ops/fr.openfisca.org/api/latest/requirements.txt"
-
 source /home/openfisca/virtualenvs/api-fr-latest/bin/activate
-pip install --requirement "$REQUIREMENTS" --upgrade --upgrade-strategy eager
+
+pip install "OpenFisca-Core[web-api]" --upgrade --upgrade-strategy eager
+pip install "OpenFisca-France" --upgrade --upgrade-strategy eager
+pip install "OpenFisca-Tracker == 0.4.0"
+
 # The current user must have been specifically allowed to run the next command
 # Use the visudo command to do so
 sudo systemctl restart openfisca-web-api-fr-latest.service
